0f4b2334234a3e0a505851347a08dc98d049fe3e
shaffaadz <dziashafa@gmail.com>
2026-05-13 12:38:28 +0700

    Ganti judul website

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold">
-        Halo, Ini Project Vercel Saya!
+        Hiiii, how are youu!
       </h1>
 
       <p className="mt-4">
-        Berhasil di-deploy lewat Git.
+        #DataScienceVibes.
       </p>
     </main>
   );
